d4eb134717e83eda2725e44993e7b24838b50ca0
Reyso <reyso.ct@gmail.com>
2021-09-03 10:17:53 -0300

    First commit

---
+++ src/com/rey/primeirojava.java
@@ -0,0 +1,25 @@
+package com.rey;
+
+import com.rey.model.Gato;
+
+public class primeirojava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Gato gato = new Gato(null, null, null);
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+	}
+
+}
+
+class Livros {
+	
+	private String nome;
+	private String npag;
+	
+	
+}
